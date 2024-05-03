f7ef888054f4e934e8a4c5bcce49ab6b6cce7988
JoseAngelCabanes <joseangelcabanes29@gmail.com>
2024-05-03 16:52:56 +0200

    formated document

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            userNameA: '',
-            userNameB: '',
-        }
+  data() {
+    return {
+      userNameA: "",
+      userNameB: "",
+    };
+  },
+  methods: {
+    userOutputA(event) {
+      this.userNameA = event.target.value;
     },
-    methods: {
-        userOutputA(event) {
-            this.userNameA = event.target.value
-        },
-        userOutputB(event) {
-            this.userNameB = event.target.value
-        },
-        showAlert() {
-            alert('Nice job!')
-        },
-    }
+    userOutputB(event) {
+      this.userNameB = event.target.value;
+    },
+    showAlert() {
+      alert("Nice job!");
+    },
+  },
 });
 
-app.mount('#assignment');
+app.mount("#assignment");
